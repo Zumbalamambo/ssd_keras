01e7ce3b3d2f60c089cc2c08c3e436acf4a76a78
Jongmoon-choi <0731nanjong@gmail.com>
2018-09-25 10:15:14 +0900

    add
    folk from https://github.com/tanakataiki/ssd_kerasV2

---
+++ testing_utils/videotest_example.py
@@ -0,0 +1,24 @@
+import keras
+import pickle
+from videotest import VideoTest
+
+import sys
+sys.path.append("..")
+from model.ssd300MobileNet import SSD
+
+input_shape = (300,300,3)
+
+# Change this if you run with other classes than VOC
+class_names = ["background", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"];
+NUM_CLASSES = len(class_names)
+
+model = SSD(input_shape, num_classes=NUM_CLASSES)
+
+# Change this path if you want to use your own trained weights
+model.load_weights('./MobileNetSSD300weights_voc_2007_class20.hdf5')
+        
+vid_test = VideoTest(class_names, model, input_shape)
+
+# To test on webcam 0, remove the parameter (or change it to another number
+# to test on that webcam)
+vid_test.run('./video/your_video.mp4')
